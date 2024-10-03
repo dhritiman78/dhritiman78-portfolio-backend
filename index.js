5303bc2ab384ee83f5fbba96e340e7df8de5162d
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2024-10-03 14:52:23 +0530

    updated cors

--- index.js
+++ index.js
@@ -13,32 +13,21 @@ app.use(express.static('assets/weblangicons'));
 app.use(express.static('assets/android_and_coding_and_tools_icons'));
 app.use(express.json()); // Ensures the request body is parsed as JSON
 
-// CORS Configuration
-const allowedOrigins = [process.env.EXPRESS_FRONTEND_URL, process.env.EXPRESS_FRONTEND_PROD_URL];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('Not allowed by CORS'), false);
-    }
-    return callback(null, true);
-  }
-}));
 
 // Tackling the CORS
-// app.use(cors({
-//     origin: process.env.EXPRESS_FRONTEND_URL, // Update this to match your actual frontend domain
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-// // Handling preflight requests
-// app.options('/api/contact', (req, res) => {
-//     res.setHeader('Access-Control-Allow-Origin', process.env.EXPRESS_FRONTEND_URL);
-//     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     res.send();
-// });
+app.use(cors({
+    origin: process.env.EXPRESS_FRONTEND_URL, // Update this to match your actual frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+// Handling preflight requests
+app.options('/api/contact', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.EXPRESS_FRONTEND_URL);
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.send();
+});
 
 app.use('/api/skills', skillsRouter);
 app.use('/api/projects', projectRouter);
